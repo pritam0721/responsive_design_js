d34630d9ad47cc8f89f7968d1b872553d63b914f
pritam0721 <pritamgayen629@gmail.com>
2024-08-12 17:43:48 +0530

    menu done with onliner uniuque value using set

---
+++ qusteions/app.js
@@ -0,0 +1,28 @@
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question')
+
+questions.forEach((question)=>{
+    const btn = question.querySelector('.question-btn')
+    btn.addEventListener('click',()=>{
+        questions.forEach((item)=>{
+            if(item !== question)
+            {
+                item.classList.remove('show-text')
+            }
+        })
+        question.classList.toggle('show-text')
+})
+})
+
+
+// traversing the dom
+
+// const btn = document.querySelectorAll('.question-btn')
+
+// btn.forEach((btn)=>{
+// btn.addEventListener('click',(e)=>{
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text')
+// })
+// })
